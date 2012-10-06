204e90342d6efc3d73dc90b92bddc943e4cbc491
Steve King <steve@dodgestar.com>
2012-10-06 09:45:57 +0100

    Prevent TaskRunner from completing early when all tasks are asynchronous

--- test/test-async.js
+++ test/test-async.js
@@ -10,24 +10,31 @@ var taskRunner, spies, then, now;
 module.exports = {
    'setUp': function() { taskRunner = new TaskRunner(); spies = {}; },
    'tearDown': function() { taskRunner = spies = null; },
+
    'test Asynchronous tasks are all started at the same time as each other': new AsyncTest(function() {
-                        // called in the right order
-                        Assertions.assert(spies.first.calledBefore(spies.second));
-                        Assertions.assert(spies.second.calledBefore(spies.third));
-
-                        // called the correct number of times
-                        Assertions.assert(spies.first.calledOnce);
-                        Assertions.assert(spies.second.calledOnce);
-                        Assertions.assert(spies.third.calledOnce);
-
-                        // called very quickly
-                        Assertions.assert((Date.now() - then) < 60);
+                        Assertions.assert('All tasks initiated before first one is complete', spies.first.calledBefore(spies.firstDone));
+                        Assertions.assert('All tasks initiated before first one is complete', spies.second.calledBefore(spies.firstDone));
+                        Assertions.assert('All tasks initiated before first one is complete', spies.third.calledBefore(spies.firstDone));
                      },
-                     function(next) {
+                     function(setUpComplete) {
                         then = Date.now();
-                        taskRunner.push(spies.first = sinon.spy(function(next) { setTimeout(next, 50); }));
-                        taskRunner.push(spies.second = sinon.spy(function(next) { setTimeout(next, 50); }));
-                        taskRunner.push(spies.third = sinon.spy(function(next) { setTimeout(next, 50); }));
-                        taskRunner.start(next);
+                        taskRunner.push(spies.first = sinon.spy(function(next) { setTimeout(spies.firstDone = sinon.spy(next), 50); }));
+                        taskRunner.push(spies.second = sinon.spy(function(next) { setTimeout(spies.secondDone = sinon.spy(next), 50); }));
+                        taskRunner.push(spies.third = sinon.spy(function(next) { setTimeout(spies.thirdDone = sinon.spy(next), 50); }));
+                        taskRunner.start(spies.done = sinon.spy(setUpComplete));
+                     }),
+
+   'test Asynchronous all need to finish before TaskRunner instance is complete': new AsyncTest(function() {
+                        Assertions.assert('All tasks initiated before first one is complete', spies.third.calledBefore(spies.firstDone));
+                        Assertions.assert('All tasks complete before taskRunner is complete', spies.firstDone.calledBefore(spies.done));
+                        Assertions.assert('All tasks complete before taskRunner is complete', spies.secondDone.calledBefore(spies.done));
+                        Assertions.assert('All tasks complete before taskRunner is complete', spies.thirdDone.calledBefore(spies.done));
+                     },
+                     function(setUpComplete) {
+                        then = Date.now();
+                        taskRunner.push(spies.first = sinon.spy(function(next) { setTimeout(spies.firstDone = sinon.spy(next), 50); }));
+                        taskRunner.push(spies.second = sinon.spy(function(next) { setTimeout(spies.secondDone = sinon.spy(next), 50); }));
+                        taskRunner.push(spies.third = sinon.spy(function(next) { setTimeout(spies.thirdDone = sinon.spy(next), 50); }));
+                        taskRunner.start(spies.done = sinon.spy(setUpComplete));
                      })
 };
